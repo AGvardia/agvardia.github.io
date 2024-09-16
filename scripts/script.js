d7851f97d84262aeab173cc3f2a97bd613a22c58
AGvardia <alex.gvardia@yandex.ru>
2024-09-16 18:17:40 +0300

    1

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("https://agvardia.github.io/data-new.json")
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelectorAll("a.phone-btn").forEach((element) => {
+        element.href = data.phone;
+        element.textContent = data.phone.replace("tel:", "");
+      });
+    });
+});
